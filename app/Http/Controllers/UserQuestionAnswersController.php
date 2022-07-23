83a32f14e08c10c7fb050f90c839c9849f125124
Hasan-Ali-Jebur <hassn.ali1212@yahoo.com>
2022-07-23 10:01:07 +0300

    add database and create sample api for testing

---
+++ app/Http/Controllers/UserQuestionAnswersController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\UserQuestionAnswers;
+use Illuminate\Http\Request;
+
+class UserQuestionAnswersController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\UserQuestionAnswers  $userQuestionAnswers
+     * @return \Illuminate\Http\Response
+     */
+    public function show(UserQuestionAnswers $userQuestionAnswers)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\UserQuestionAnswers  $userQuestionAnswers
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(UserQuestionAnswers $userQuestionAnswers)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\UserQuestionAnswers  $userQuestionAnswers
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, UserQuestionAnswers $userQuestionAnswers)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\UserQuestionAnswers  $userQuestionAnswers
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(UserQuestionAnswers $userQuestionAnswers)
+    {
+        //
+    }
+}
